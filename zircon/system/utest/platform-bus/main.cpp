87ea3ae831a83f934ab626faa59149057dcf087a
Todd Eisenberger <teisenbe@google.com>
2019-03-06 16:11:36 -0800

    [devcoordinator] Check that component binding happens
    This verifies that when a composite device component is matched, the
    component driver binds.
    
    Issue: ZX-2648
    Change-Id: I66ad66c52ea750c6977e23d98fa4794f19bed2bd

--- zircon/system/utest/platform-bus/main.cpp
+++ zircon/system/utest/platform-bus/main.cpp
@@ -80,11 +80,27 @@ bool enumeration_test() {
                                    zx::deadline_after(zx::sec(5)), &fd),
               ZX_OK);
 
-    EXPECT_EQ(RecursiveWaitForFile(devmgr.devfs_root(), "sys/platform/11:01:1/child-1/child-2",
+    EXPECT_EQ(RecursiveWaitForFile(devmgr.devfs_root(), "sys/platform/11:01:1/child-1/child-2-top",
+                                   zx::deadline_after(zx::sec(5)), &fd),
+              ZX_OK);
+    EXPECT_EQ(RecursiveWaitForFile(devmgr.devfs_root(),
+                                   "sys/platform/11:01:1/child-1/child-2-top/child-2",
+                                   zx::deadline_after(zx::sec(5)), &fd),
+              ZX_OK);
+    EXPECT_EQ(RecursiveWaitForFile(devmgr.devfs_root(), "sys/platform/11:01:1/child-1/child-3-top",
+                                   zx::deadline_after(zx::sec(5)), &fd),
+              ZX_OK);
+    EXPECT_EQ(RecursiveWaitForFile(devmgr.devfs_root(),
+                                   "sys/platform/11:01:1/child-1/child-3-top/child-3",
                                    zx::deadline_after(zx::sec(5)), &fd),
               ZX_OK);
 
-    EXPECT_EQ(RecursiveWaitForFile(devmgr.devfs_root(), "sys/platform/11:01:1/child-1/child-3",
+    EXPECT_EQ(RecursiveWaitForFile(devmgr.devfs_root(),
+                                   "sys/platform/11:01:1/child-1/child-2-top/child-2/component",
+                                   zx::deadline_after(zx::sec(5)), &fd),
+              ZX_OK);
+    EXPECT_EQ(RecursiveWaitForFile(devmgr.devfs_root(),
+                                   "sys/platform/11:01:1/child-1/child-3-top/child-3/component",
                                    zx::deadline_after(zx::sec(5)), &fd),
               ZX_OK);
 
@@ -93,8 +109,14 @@ bool enumeration_test() {
     EXPECT_EQ(fstatat(dirfd, "sys/platform/test-board", &st, 0), 0);
     EXPECT_EQ(fstatat(dirfd, "sys/platform/11:01:1", &st, 0), 0);
     EXPECT_EQ(fstatat(dirfd, "sys/platform/11:01:1/child-1", &st, 0), 0);
-    EXPECT_EQ(fstatat(dirfd, "sys/platform/11:01:1/child-1/child-2", &st, 0), 0);
-    EXPECT_EQ(fstatat(dirfd, "sys/platform/11:01:1/child-1/child-3", &st, 0), 0);
+    EXPECT_EQ(fstatat(dirfd, "sys/platform/11:01:1/child-1/child-2-top", &st, 0), 0);
+    EXPECT_EQ(fstatat(dirfd, "sys/platform/11:01:1/child-1/child-3-top", &st, 0), 0);
+    EXPECT_EQ(fstatat(dirfd, "sys/platform/11:01:1/child-1/child-2-top/child-2", &st, 0), 0);
+    EXPECT_EQ(fstatat(dirfd, "sys/platform/11:01:1/child-1/child-3-top/child-3", &st, 0), 0);
+    EXPECT_EQ(fstatat(dirfd, "sys/platform/11:01:1/child-1/child-2-top/child-2/component", &st, 0),
+              0);
+    EXPECT_EQ(fstatat(dirfd, "sys/platform/11:01:1/child-1/child-3-top/child-3/component", &st, 0),
+              0);
 
     END_TEST;
 }
